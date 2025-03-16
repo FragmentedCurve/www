baaf77192840ab12f9de3b99b77b769ad1c3730a
Paco Pascal <me@pacopascal.com>
2025-03-16 08:24:39 -0400

    Don't publish

--- data/articles.lisp
+++ data/articles.lisp
@@ -48,8 +48,8 @@
 
 
 ;; Farewell to a Childhood Hero
-(post :id "20230719_mitnick" :date "20230719"
-      :title "Farewell to a Childhood Hero" :tag "kevin mitnick")
+;;(post :id "20230719_mitnick" :date "20230719"
+;;      :title "Farewell to a Childhood Hero" :tag "kevin mitnick")
 
 ;; Nesticle: Part 1
 (post :id "20230528_nesticle_1" :date "20230528"
@@ -64,8 +64,8 @@
 ;;      :title "Wish List: 2022" :tag "wishlist")
 
 ;; I Met a Man Today
-(post :id "20221016_i_met_a_man_today" :date "20221016"
-      :title "I Met a Man Today" :tag "fiction short")
+;;(post :id "20221016_i_met_a_man_today" :date "20221016"
+;;      :title "I Met a Man Today" :tag "fiction short")
 
 ;; I Want a Beard
 ;;(post :id "20130928_i_want_a_beard" :date "20130928"
@@ -80,8 +80,8 @@
       :title "Porting NESticle for Fun and Profit: Part 0" :tag "nes emulation")
 
 ;; Using Age to Publicly Post Sensitive Data
-(post :id "20220611_public_data" :date "20220611"
-      :title "Using Age to Publicly Post Sensitive Data" :tag "unix linux gpg age cryptography")
+;;(post :id "20220611_public_data" :date "20220611"
+;;      :title "Using Age to Publicly Post Sensitive Data" :tag "unix linux gpg age cryptography")
 
 ;; Over the Wire: Behemoth
 (post :id "20220517_behemoth" :date "20220517"
